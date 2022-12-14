94c19298ac6759f41383d07ea699b294613009c3
createyouGitHub <118916290+createyouGitHub@users.noreply.github.com>
2022-12-14 14:58:47 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,70 +1,73 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "X") {
         左右 = value
-    } else {
+    } else if (name == "Y") {
         前后 = value
-    }
-    if (左右 < 300 && 左右 > -300 && 前后 > 300) {
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 1023)
-        pins.analogWritePin(AnalogPin.P3, 0)
-        radio.sendNumber(8)
-    } else if (左右 < 300 && 左右 > -300 && 前后 < 300) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, 1023)
-        radio.sendNumber(2)
-    } else if (前后 < 300 && 前后 > -300 && 左右 < -300) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 1023)
-        pins.analogWritePin(AnalogPin.P3, 0)
-        radio.sendNumber(4)
-    } else if (左右 < 300 && 左右 > -300 && 前后 > 300) {
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, 1023)
-        radio.sendNumber(6)
-    } else if (左右 > 300 && 前后 > 300) {
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 500)
-        pins.analogWritePin(AnalogPin.P3, 0)
-        radio.sendNumber(9)
-    } else if (左右 < -300 && 前后 > 300) {
-        pins.analogWritePin(AnalogPin.P0, 500)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 1023)
-        pins.analogWritePin(AnalogPin.P3, 0)
-        radio.sendNumber(7)
-    } else if (左右 > 300 && 前后 < -300) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 1023)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, 500)
-        radio.sendNumber(3)
-    } else if (左右 < -300 && 前后 < -300) {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 500)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, 1023)
-        radio.sendNumber(1)
+    } else if (name == "AD") {
+        车臂 = value
     } else {
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, 0)
-        basic.showIcon(IconNames.Silly)
-        radio.sendNumber(5)
+        机械臂 = 0
+    }
+    if (前后 > 300) {
+        if (左右 > 1000) {
+            pins.analogWritePin(AnalogPin.P12, 600)
+            pins.analogWritePin(AnalogPin.P15, 1023)
+        } else if (左右 < -1000) {
+            pins.analogWritePin(AnalogPin.P12, 1023)
+            pins.analogWritePin(AnalogPin.P15, 600)
+        } else {
+            pins.analogWritePin(AnalogPin.P12, 1023)
+            pins.analogWritePin(AnalogPin.P15, 1023)
+        }
+    } else if (前后 < -300) {
+        if (左右 > 1000) {
+            pins.analogWritePin(AnalogPin.P13, 600)
+            pins.analogWritePin(AnalogPin.P16, 1023)
+        } else if (左右 < -1000) {
+            pins.analogWritePin(AnalogPin.P13, 1023)
+            pins.analogWritePin(AnalogPin.P16, 600)
+        } else {
+            pins.analogWritePin(AnalogPin.P13, 1023)
+            pins.analogWritePin(AnalogPin.P16, 1023)
+        }
+    } else if (前后 < 300 && 前后 > -300) {
+        if (左右 > 1000) {
+            pins.analogWritePin(AnalogPin.P13, 1023)
+            pins.analogWritePin(AnalogPin.P15, 1023)
+        } else if (左右 < -1000) {
+            pins.analogWritePin(AnalogPin.P12, 1023)
+            pins.analogWritePin(AnalogPin.P16, 1023)
+        } else {
+            pins.analogWritePin(AnalogPin.P12, 0)
+            pins.analogWritePin(AnalogPin.P13, 0)
+            pins.analogWritePin(AnalogPin.P15, 0)
+            pins.analogWritePin(AnalogPin.P16, 0)
+        }
+    }
+    if (车臂 >= 300) {
+        pins.servoWritePin(AnalogPin.P11, pins.map(
+        车臂,
+        -734,
+        734,
+        0,
+        180
+        ))
+    } else if (车臂 <= -300) {
+        pins.servoWritePin(AnalogPin.P10, pins.map(
+        车臂,
+        -734,
+        734,
+        0,
+        180
+        ))
     }
 })
+let 机械臂 = 0
+let 车臂 = 0
 let 前后 = 0
 let 左右 = 0
 basic.showIcon(IconNames.Silly)
-radio.setGroup(1)
+radio.setGroup(255)
 basic.forever(function () {
 	
 })
